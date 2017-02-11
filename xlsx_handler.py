f8db9a785a04b053fbf0ae18f8a76025dba1e4c1
Nicola Demo <demo.nicola@gmail.com>
2017-02-11 14:25:13 +0100

    Add saving method for xlsx file

--- xlsx_handler.py
+++ xlsx_handler.py
@@ -1,4 +1,4 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 
 
 def read_xlsx(filename, intestation=True):
@@ -12,11 +12,27 @@ def read_xlsx(filename, intestation=True):
 
 	for name, info in main_sheet.iter_rows(min_row=minimum_row):
 		data[name.value] = [
-			x.replace("_x000D_", "").split(':') 
-			for x in info.value.split('\n') if x
+			x.split(':') 
+			for x in info.value.replace("_x000D_", "\n").split('\n') if x
 		]
 
 	return data
 
 def save_xlsx(filename, data, intestation=None):
-	pass
+	
+	wb = Workbook()
+	main_sheet = wb.active
+	main_sheet.title = filename
+
+	starting_row = 1
+	if intestation:
+		main_sheet.cell(row=starting_row, column=1).value = instestation[0]
+		main_sheet.cell(row=starting_row, column=2).value = instestation[1]
+		starting_row += 1
+
+	for index, name in enumerate(data.keys(), start=1):
+		info_string = '\n'.join([': '.join(x) for x in data[name]])
+		main_sheet.cell(row=index, column=1).value = name
+		main_sheet.cell(row=index, column=2).value = info_string
+
+	wb.save(filename)
