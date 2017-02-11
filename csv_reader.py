a052129a4a472e668ebdc0f5fdfc61cfdd810971
Nicola Demo <demo.nicola@gmail.com>
2017-02-11 10:56:16 +0100

    Add method to write csv file

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,6 @@
 import csv
 
-def parse_csv(filename, intestation=True):
+def read_csv(filename, intestation=True):
 
 	data = dict()
 
@@ -11,4 +11,18 @@ def parse_csv(filename, intestation=True):
 			next(reader)
 
 		for name, info in reader:
-			data[name] = [x.split(':') for x in info.split('\n')]
+			data[name] = [x.split(':') for x in info.split('\n') if x]
+
+	return data
+
+def save_csv(filename, data, intestation=None):
+
+	with open(filename, 'w') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',')
+
+		if intestation:
+			writer.writerow[instestation]
+
+		for name in data:
+			info_string = '\n'.join([': '.join(x) for x in data[name]])
+			writer.writerow([name, info_string])
