5c91752295998705c3edef699c663810d2f5c3ef
Nicola Demo <demo.nicola@gmail.com>
2017-02-13 11:46:13 +0100

    Add similar string matching in field format

--- dataset.py
+++ dataset.py
@@ -1,15 +1,32 @@
 import sys
 import re
 
+from difflib import get_close_matches
 from file_handler import read_file, save_file
+
+class Info(object):
+	SHARE_NAME = 	'Share Owner Name'
+	SALARY_NO =		'Salary No'
+	DELEGATE_NAME =	'Delegate Owner Name'
+	DELEGATE_SALARY = 'Delegate Salary No'
+	DATA_CLASSIF =	'Data Classification'
 
 class Dataset(object):
 	"""
 	Documentation
+
 	"""
+
 	def __init__(self):
 		self.data          = None
 		self.invalid_chars = re.compile(r'[\t\n\r\f\v\#]*')
+		self.valid_fields  = [
+			Info.SHARE_NAME,
+			Info.SALARY_NO,
+			Info.DELEGATE_NAME,
+			Info.DELEGATE_SALARY,
+			Info.DATA_CLASSIF
+		]
 
 	def read(self, input_file):
 		"""
@@ -23,15 +40,19 @@ class Dataset(object):
 
 			new_info = []
 			for info in self.data[name]:
+			
+				valid_info = get_close_matches(info[0], self.valid_fields, n=1)
 				
-				if len(info) != 2:
-					continue
-				
-				tmp = [self._clean_string(i) for i in info]
-				if all(tmp):
-					new_info.append(tmp)
+				if valid_info:
+					try:
+						valid_value = self._clean_string(info[1])
+					except IndexError:
+						valid_value = ""
 
+					new_info.append([valid_info[0], valid_value])
+					
 			self.data[name] = new_info
+
 
 	def save(self, output_file):
 		"""
